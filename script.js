412d82ce8fe4f52d7f62fd759b7845e605d5d1b8
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-03-17 10:35:37 +0100

    minor changes

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 //! Email
 
-const emailNotification  = document.querySelector('.email-validity-notification')
-const emailInput         = document.querySelector('#email')
-const emailSubmit        = document.querySelector('.email-submit')
-const emailForm          = document.querySelector('.email-form')
+const emailNotification = document.querySelector('.email-validity-notification')
+const emailInput = document.querySelector('#email')
+const emailSubmit = document.querySelector('.email-submit')
+const emailForm = document.querySelector('.email-form')
 
-emailSubmit.addEventListener('click', () => {
-  if(!emailInput.checkValidity()){
+emailInput.addEventListener('focusout', () => {
+  if (!emailInput.checkValidity()) {
     emailNotification.innerHTML = 'Malformed Email'
     emailNotification.style.color = 'red'
-  } else if(emailInput.value === 'example@mail.com'){
+  } else if (emailInput.value === 'example@mail.com') {
     emailNotification.innerHTML = 'email already registered'
     emailNotification.style.color = 'red'
   } else {
@@ -28,19 +28,19 @@ emailForm.addEventListener('submit', (e) => {
 //! Password
 
 const showPasswordButtons = document.querySelectorAll(".show-password-btn")
-const password            = document.querySelector("#password")
-const confirmPassword     = document.querySelector("#confirm-password")
-const matchNotification   = document.querySelector('#match-notification')
-const uppercase           = document.querySelector('.uppercase')
-const lowercase           = document.querySelector('.lowercase')
-const number              = document.querySelector('.number')
-const specialChar         = document.querySelector('.special-character')
-const chars               = document.querySelector('.characters')
-const strengthLines       = document.querySelector('.strength-lines')
-const lines               = document.querySelectorAll('.line')
+const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirm-password")
+const matchNotification = document.querySelector('#match-notification')
+const uppercase = document.querySelector('.uppercase')
+const lowercase = document.querySelector('.lowercase')
+const number = document.querySelector('.number')
+const specialChar = document.querySelector('.special-character')
+const chars = document.querySelector('.characters')
+const strengthLines = document.querySelector('.strength-lines')
+const lines = document.querySelectorAll('.line')
 
 // At least one LOWERCASE character:
-const lowerCasePattern  = /^(?=.*[a-z]).+$/;
+const lowerCasePattern = /^(?=.*[a-z]).+$/;
 
 // At least one UPPERCASE character:
 const upperCasePattern = /^(?=.*[A-Z]).+$/;
@@ -55,22 +55,22 @@ const specialCharacterPattern = /([-+=_!@#$%^&*.,;:'\"<>/?`~\¦\°\§\´\¨\[\]\
 const characterCountPattern = /^.{6,}/;
 
 
-showPasswordButtons.forEach((button) => button.addEventListener('click', function(e) {
-    if(password.type === 'password' && confirmPassword.type === 'password') {
-        password.type = 'text'
-        confirmPassword.type = 'text'
-        showPasswordButtons.forEach((button) => button.innerHTML = "Hide")
-    } else {
-        password.type = 'password'
-        confirmPassword.type = 'password'
-        showPasswordButtons.forEach((button) => button.innerHTML = "Show")
-    }
+showPasswordButtons.forEach((button) => button.addEventListener('click', function (e) {
+  if (password.type === 'password' && confirmPassword.type === 'password') {
+    password.type = 'text'
+    confirmPassword.type = 'text'
+    showPasswordButtons.forEach((button) => button.innerHTML = "Hide")
+  } else {
+    password.type = 'password'
+    confirmPassword.type = 'password'
+    showPasswordButtons.forEach((button) => button.innerHTML = "Show")
+  }
 
-    password.focus();
+  password.focus();
 }))
 
-confirmPassword.addEventListener('keyup', () => {  
-  if(password.value === confirmPassword.value){
+confirmPassword.addEventListener('keyup', () => {
+  if (password.value === confirmPassword.value) {
     matchNotification.innerHTML = 'Your passwords match!'
     matchNotification.style.color = 'green'
   } else {
@@ -92,7 +92,7 @@ password.addEventListener('keyup', () => {
 })
 
 const toggleRequirement = (pwd, regex, el) => {
-  if(regex.test(pwd.value)){
+  if (regex.test(pwd.value)) {
     el.style.color = 'green'
   } else {
     el.style.color = 'red'
@@ -102,7 +102,7 @@ const toggleRequirement = (pwd, regex, el) => {
 const testPasswordStrength = (value) => {
   const strongRegex = new RegExp(
     '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[=/\()%ยง!@#$%^&*])(?=.{8,})'
-    ),
+  ),
     mediumRegex = new RegExp(
       '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'
     );
@@ -122,8 +122,8 @@ const styleStrengthLine = (color, value) => {
     line.classList.add("bg-transparent")
   })
 
-  if(value) {
-    if(color === "red") {
+  if (value) {
+    if (color === "red") {
       lines[0].classList.remove("bg-transparent")
       lines[0].classList.add("bg-red")
     } else if (color === "orange") {
