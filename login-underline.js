0d4d18457e16d172868c44b421bc3d2a4d2f8706
DevDuki <dusan-misic@hotmail.com>
2021-04-14 17:33:08 +0200

    added forgot password button and notifications for login success/failure

--- login-underline.js
+++ login-underline.js
@@ -5,7 +5,9 @@ const email2 = "example@mail.com";
 const pw2 = 'P4$$word'
 
 const loginForm2 = document.querySelector('.login-form-underline')
-const loginNotification2 = document.querySelector('.login-validity-notification-underline')
+const notificationContainer2 = document.querySelector('.login-validity-notification-underline')
+const notificationMessage2 = document.querySelector('.login-validity-notification-underline p')
+const notificationIcon2 = document.querySelector('.login-validity-notification-underline i')
 const emailNotification2 = document.querySelector('.email-validity-notification-underline')
 const emailInput2 = document.querySelector('#email-underline')
 const passwordInput2 = document.querySelector('#password-underline')
@@ -15,11 +17,17 @@ const showPwBtn2 = document.querySelector('#show-pw-btn-underline')
 loginForm2.addEventListener('submit', (e) => {
   e.preventDefault()
   if(!(passwordInput2.value === pw2 && emailInput2.value === email2)){
-    loginNotification2.innerHTML = 'Wrong credentials'
-    loginNotification2.style.color = 'rgb(210, 16, 16)'
+    notificationMessage2.innerHTML = `Sorry, we couldn't match your request. Your E-Mail or Password must be wrong.`
+    notificationContainer2.style.display = 'flex'
+    notificationContainer2.style.backgroundColor = 'var(--red-hsl)'
+    notificationContainer2.style.color = 'var(--white-hsl)'
+    notificationIcon2.classList.add('fa-exclamation-circle')
   } else {
-    loginNotification2.innerHTML = 'Login successful!'
-    loginNotification2.style.color = 'green'
+    notificationMessage2.innerHTML = `Login successful!`
+    notificationContainer2.style.display = 'flex'
+    notificationContainer2.style.backgroundColor = 'var(--dark-green-hsl)'
+    notificationContainer2.style.color = 'var(--white-hsl)'
+    notificationIcon2.classList.add('fa-check-circle')
   }
 
   if(showPwBtn2.classList.contains('disabled')){
--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const email = 'example@mail.com'
 const pw = 'P4$$word'
 
 const loginForm = document.querySelector('.login-form')
-const loginNotification = document.querySelector('.login-validity-notification')
+const notificationContainer = document.querySelector('.login-validity-notification')
+const notificationMessage = document.querySelector('.login-validity-notification p')
+const notificationIcon = document.querySelector('.login-validity-notification i')
 const emailNotification = document.querySelector('.email-validity-notification')
 const emailInput = document.querySelector('#email')
 const passwordInput = document.querySelector('#password')
@@ -15,11 +17,17 @@ const showPwBtn = document.querySelector('#show-pw-btn')
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault()
   if (!(passwordInput.value === pw && emailInput.value === email)) {
-    loginNotification.innerHTML = 'Wrong credentials'
-    loginNotification.style.color = 'var(--red-hsl)'
+    notificationMessage.innerHTML = `Sorry, we couldn't match your request. Your E-Mail or Password must be wrong.`
+    notificationContainer.style.display = 'flex'
+    notificationContainer.style.backgroundColor = 'var(--red-hsl)'
+    notificationContainer.style.color = 'var(--white-hsl)'
+    notificationIcon.classList.add('fa-exclamation-circle')
   } else {
-    loginNotification.innerHTML = 'Login successful!'
-    loginNotification.style.color = 'var(--dark-green-hsl)'
+    notificationMessage.innerHTML = `Login successful!`
+    notificationContainer.style.display = 'flex'
+    notificationContainer.style.backgroundColor = 'var(--dark-green-hsl)'
+    notificationContainer.style.color = 'var(--white-hsl)'
+    notificationIcon.classList.add('fa-check-circle')
   }
 
   if (showPwBtn.classList.contains('disabled')) {
