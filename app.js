6db2289c0663b474d8c7a6909dfdccced504ca65
DevDuki <dusan-misic@hotmail.com>
2021-06-02 14:41:46 +0200

    simple projector pattern

---
+++ app.js
@@ -0,0 +1,17 @@
+import {formProjector, inputProjector} from './projector/projectors.js'
+
+const root = document.querySelector('.form')
+
+// Creating input fields via inputProjector
+const emailInput = document.querySelector('.email')
+const emailInputProjected = inputProjector(emailInput, 'email', 'text', 'example@mail.com', '.+@.+\\..+', 'Invalid Mail')
+
+const passwordInput = document.querySelector('.password')
+const passwordInputProjected = inputProjector(passwordInput, 'password', 'password', 'P4$$word')
+
+const dateInput = document.querySelector('.date')
+const dateInputProjected = inputProjector(dateInput, 'date', 'date')
+
+
+// Creating form via formProjector with the recent created Inputfields
+formProjector(root, 'Test', [emailInputProjected, passwordInputProjected, dateInputProjected])
